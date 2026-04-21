ce0a4634dba12c37bf128408f5f530a385408665
Dashiell Elliott <dashiell.elliott@yahoo.com>
2026-04-21 16:39:23 -0400

    fix for real this time?

--- src/template_states/States.cpp
+++ src/template_states/States.cpp
@@ -1,25 +1,32 @@
-#define TEMPLATE_STATES_OVERRIDE
-#include "States.h"
-#include "../State.h"
+#ifdef __has_include
+  #if !__has_include("../states/States.h")
+    #define TEMPLATE_STATES_OVERRIDE
+  #endif
+#endif
+
+#if defined(TEMPLATE_STATES_OVERRIDE)
+  #include "States.h"
+  #include "../State.h"
 
 
-StateInitFunc initFuncs[NUM_STATES] = {};
-StateLoopFunc loopFuncs[NUM_STATES] = {};
+  StateInitFunc initFuncs[NUM_STATES] = {};
+  StateLoopFunc loopFuncs[NUM_STATES] = {};
 
-void initStateMap() {
-  initFuncs[PRELAUNCH] = &prelaunchInit;
-  initFuncs[BOOST] = &boostInit;
-  initFuncs[COAST] = &coastInit;
-  initFuncs[DROGUE_DESCENT] = &drogueDescentInit;
-  initFuncs[MAIN_DESCENT] = &mainDescentInit;
-  initFuncs[RECOVERY] = &recoveryInit;
-  initFuncs[ABORT] = &abortInit;
+  void initStateMap() {
+    initFuncs[PRELAUNCH] = &prelaunchInit;
+    initFuncs[BOOST] = &boostInit;
+    initFuncs[COAST] = &coastInit;
+    initFuncs[DROGUE_DESCENT] = &drogueDescentInit;
+    initFuncs[MAIN_DESCENT] = &mainDescentInit;
+    initFuncs[RECOVERY] = &recoveryInit;
+    initFuncs[ABORT] = &abortInit;
 
-  loopFuncs[PRELAUNCH] = &prelaunchLoop;
-  loopFuncs[BOOST] = &boostLoop;
-  loopFuncs[COAST] = &coastLoop;
-  loopFuncs[DROGUE_DESCENT] = &drogueDescentLoop;
-  loopFuncs[MAIN_DESCENT] = &mainDescentLoop;
-  loopFuncs[RECOVERY] = &recoveryLoop;
-  loopFuncs[ABORT] = &abortLoop;
-}
+    loopFuncs[PRELAUNCH] = &prelaunchLoop;
+    loopFuncs[BOOST] = &boostLoop;
+    loopFuncs[COAST] = &coastLoop;
+    loopFuncs[DROGUE_DESCENT] = &drogueDescentLoop;
+    loopFuncs[MAIN_DESCENT] = &mainDescentLoop;
+    loopFuncs[RECOVERY] = &recoveryLoop;
+    loopFuncs[ABORT] = &abortLoop;
+  }
+#endif
